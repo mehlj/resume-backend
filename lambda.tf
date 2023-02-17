d5ceba343172221d463fd5b3fd578a727d4aae59
Justen Mehl <justen.t.mehl.civ@us.navy.mil>
2023-02-17 10:29:28 -0500

    add lambda

--- lambda.tf
+++ lambda.tf
@@ -1,13 +1,9 @@
-// TODO - complete (besides role line)
+resource "aws_lambda_function" "function" {
+  filename         = "lambda-handler.zip"
+  function_name    = "resume-visitor-counter"
+  source_code_hash = filebase64sha256("lambda-handler.zip")
+  role             = aws_iam_role.lambda-role.arn
 
-# resource "aws_lambda_function" "function" {
-#   function_name = "MyLambdaFunction"
-
-#   role             = aws_iam_role.lambda-role.arn
-#   handler          = "index.handler"
-#   filename         = "${path.module}/assets/index.js.zip"
-
-#   runtime     = "nodejs16.x"
-#   memory_size = 128
-#   timeout     = 3
-# }
+  handler = "bootstrap"
+  runtime = "go1.x"
+}
